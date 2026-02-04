7b2eed738d09409c892a5317a1f99790af672a07
Jean Gonçalves <jean.gnc@gmail.com>
2026-02-04 14:08:52 -0300

    Fixes override

--- lua/neorg/modules/external/indent/override.lua
+++ lua/neorg/modules/external/indent/override.lua
@@ -1,21 +1,20 @@
 local M = {}
 
---- Disable core.esupports.indent by replacing its functions with our own.
----@param core_indent table the core.esupports.indent module table
+--- Disable core.esupports.indent by replacing its public functions with our own.
+--- modules.await passes the module's public table directly.
+---@param core_indent_public table the core.esupports.indent module's public table
 ---@param render_buffer_fn function(bufid) re-render a buffer's indentation
 ---@param indentexpr_fn function() indentexpr replacement
-function M.disable_core_indent(core_indent, render_buffer_fn, indentexpr_fn)
-    core_indent.on_event = function() end
-
-    core_indent.public.reindent_range = function(buffer, _, _)
+function M.disable_core_indent(core_indent_public, render_buffer_fn, indentexpr_fn)
+    core_indent_public.reindent_range = function(buffer, _, _)
         if vim.api.nvim_buf_is_valid(buffer) then
             render_buffer_fn(buffer)
         end
     end
 
-    core_indent.public.buffer_set_line_indent = function() end
+    core_indent_public.buffer_set_line_indent = function() end
 
-    core_indent.public.indentexpr = indentexpr_fn
+    core_indent_public.indentexpr = indentexpr_fn
 end
 
 return M
